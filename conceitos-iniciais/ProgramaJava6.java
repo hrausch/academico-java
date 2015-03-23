1f62d8ba5452163a6fe681b4650ddcaef02ce7cd
herbertrf <hrausch@gmail.com>
2015-03-23 14:18:36 -0300

    Finalizando o código exemplo de scanner, acessando um caracter da string.

--- conceitos-iniciais/ProgramaJava6.java
+++ conceitos-iniciais/ProgramaJava6.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class ProgramaJava6{
 
-
-	public static void main(String [] args){
-
+	public void lerNome(){
+	
 		// Instancia um objeto scanner para obter entrada a partir do console
 		Scanner input = new Scanner( System.in );
 
@@ -15,12 +14,44 @@ public class ProgramaJava6{
 
 		for(int i =0; i < tamanho; i++){
 
-			System.out.println("Letra: " + nome[i] + " - posição no vetor: " + i);
+			char caracter = nome.charAt(i);
+			System.out.println("Letra: " + caracter + " - posicao no vetor: " + i);
 		}
-
-
-
+	}
 	
+	public void lerNumero(){
+	
+		// Instancia um objeto scanner para obter entrada a partir do console
+		Scanner input = new Scanner( System.in );
+
+		System.out.println("Qual o seu ano de nascimento:");
+		String texto = input.next(); 
+
+		int tamanho = texto.length();
+		
+		int significativo = 1000;
+		int ano = 0;
+
+		for(int i =0; i < tamanho; i++){
+			//obtem o caracter de posicao i da string
+			char caracter = texto.charAt(i);
+			//converte um caracter em inteiro
+			int numero = Character.getNumericValue(caracter);
+			
+			numero = numero * significativo;
+			ano += numero;
+			significativo = significativo / 10;
+			
+		}
+		
+		System.out.println("\n** O ano digitado foi: " + ano + " ** A sua idade eh: " + (2015-ano) );
+	}
+
+
+	public static void main(String [] args){
+			ProgramaJava6 objetoPrograma6 = new ProgramaJava6();			
+			objetoPrograma6.lerNome();
+			objetoPrograma6.lerNumero();	
 	}
 
 
